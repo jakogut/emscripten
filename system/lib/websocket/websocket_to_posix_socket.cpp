692ddb64e2e1ecef1ebe638ecbdf6cce93087770
Joseph Kogut <joseph.kogut@gmail.com>
2019-04-29 20:42:56 -0700

    fixup gai_strerror

--- system/lib/websocket/websocket_to_posix_socket.cpp
+++ system/lib/websocket/websocket_to_posix_socket.cpp
@@ -1006,10 +1006,27 @@ int getnameinfo(const struct sockaddr *address, socklen_t address_len, char *hos
   return ret;
 }
 
-// TODO:
 const char *gai_strerror(int)
 {
-	return "gai_strerror";
+  const char msgs[] =
+	  "Invalid flags\0"
+	  "Name does not resolve\0"
+	  "Try again\0"
+	  "Non-recoverable error\0"
+	  "Unknown error\0"
+	  "Unrecognized address family or unvalid length\0"
+	  "Unrecognized socket type\0"
+	  "Unrecognized service\0"
+	  "Uknown error\0"
+	  "Out of memory\0"
+	  "System error\0"
+	  "Overflow\0"
+	  "\0Unknown error";
+
+  const char *s;
+  for (s=msgs, ecode++; ecode && *s; ecode++, s++) for (; *s; s++);
+  if (!*s) s++;
+  return s;
 }
 
 
